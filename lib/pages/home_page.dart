034b8f317534c8620ddd7478baeb3354c5374fa5
spock <372842848@qq.com>
2021-01-31 22:53:00 +0800

    feat: 首页轮播图

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,11 +7,34 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePage extends State<HomePage> {
+  List _imageUrls = [
+    'https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3118158831,1502048080&fm=26&gp=0.jpg',
+    'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=391552132,10057802&fm=26&gp=0.jpg',
+    'https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3551370719,1936559374&fm=26&gp=0.jpg'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('home 首页'),
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 180,
+              child: Swiper(
+                  itemCount: _imageUrls.length,
+                  autoplay: true,
+                  itemBuilder: (BuildContext content, int index) {
+                    return (Image.network(
+                      _imageUrls[index],
+                      fit: BoxFit.fill,
+                    ));
+                  },
+                  pagination: SwiperPagination(),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
